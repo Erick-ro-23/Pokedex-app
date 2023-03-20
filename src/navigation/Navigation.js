5d93519bd0d210cfa62c8b54137e24a6c707a9d6
Erick-ro-23 <76449897+Erick-ro-23@users.noreply.github.com>
2023-03-21 00:55:36 +0100

    Diseñando la Tap navigation con iconos

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // import { NavigationContainer } from '@react-navigation/native';
-
+import Icon from 'react-native-vector-icons/FontAwesome5';
 //Screens
 import FavoriteScreen from '../screens/Favorite';
 import PokedexScreen from '../screens/Pokedex';
@@ -13,9 +13,15 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name='Favorite' component={FavoriteScreen} />
+            <Tab.Screen name='Favorite' component={FavoriteScreen} options={{
+                tabBarLabel: "Favoritos",
+                tabBarIcon: ({color, size}) => <Icon name="heart" color= {color} size={size}/>,
+                }}/>
             <Tab.Screen name='Pokedex' component={PokedexScreen} />
-            <Tab.Screen name='Account' component={Account} />
+            <Tab.Screen name='Account' component={Account} options={{
+                tabBarLabel: "Mi Cuenta",
+                tabBarIcon: ({color,size}) => <Icon name="user" color={color} size={size}/>
+        }}/>
         </Tab.Navigator>
     )
 }
