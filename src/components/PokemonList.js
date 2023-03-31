a3b9e0c9d25e403ac007736b7f4c3a58930596cb
Erick-ro-23 <76449897+Erick-ro-23@users.noreply.github.com>
2023-03-31 18:06:27 +0200

    Renderizando la PokemonCard

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text,FlatList } from 'react-native'
+import { StyleSheet, Text,FlatList } from 'react-native';
+
+import PokemonCard from './PokemonCard';
+
 
 export default function PokemonList(props) {
-    const { pokemons} = props;
+    const {pokemons} = props;
     return (
     <FlatList 
         data={pokemons}
         numColumns={2}
         showsVerticalScrollIndicator={false}
         keyExtractor={(pokemon) => String(pokemon.id)}
-        renderItem={({item})=> <Text>{item.name}</Text>}
+        renderItem={({item})=> <PokemonCard pokemon= {item}/>}
         contentContainerStyle={styles.flatListContentContainer}
     />
     );
